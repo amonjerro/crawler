e94eb9431a49eacce51d264e494d79329703d38e
Bruno Tafur <btafur@Brunos-MacBook-Pro.local>
2017-10-11 13:59:13 -0500

    i dont know

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ app.use(function(req,res){
 	res.type('text/html');
 	res.status(404);
 	res.render('404');
-});
+})
 
 app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
-});
+})
 
 
 app.listen(app.get('port'),function(){
 	console.log('Running - Ctrl+C to stop application');
-});
+})
